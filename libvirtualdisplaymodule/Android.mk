e5effacf92b55d86a20c5a30227fc789001093c3
Salih Çağlar <111198755+salih4187@users.noreply.github.com>
2025-10-28 15:20:12 +0300

    Update virtual display module for new bsp

--- libvirtualdisplaymodule/Android.mk
+++ libvirtualdisplaymodule/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,35 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(filter-out exynos5,$(TARGET_BOARD_PLATFORM)),)
-
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libexynosutils libexynosv4l2 libsync libexynosdisplay libvirtualdisplay
-
-LOCAL_CFLAGS += -DUSES_VIRTUAL_DISPLAY
-
-LOCAL_C_INCLUDES := \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-	$(LOCAL_PATH)/../../exynos5/include \
-	$(TOP)/hardware/samsung_slsi/exynos/include \
-	$(TOP)/hardware/samsung_slsi/exynos/libexynosutils \
-	$(TOP)/hardware/samsung_slsi/$(TARGET_SOC)/include \
-	$(TOP)/hardware/samsung_slsi/$(TARGET_SOC)/libhwcmodule \
-	$(TOP)/hardware/samsung_slsi/exynos/libhwc \
-	$(TOP)/hardware/samsung_slsi/exynos/libhwcutils \
-	$(TOP)/hardware/samsung_slsi/exynos/libexynosdisplay \
-	$(TOP)/hardware/samsung_slsi/exynos/libvirtualdisplay
-
-LOCAL_ADDITIONAL_DEPENDENCIES += \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
-LOCAL_SRC_FILES := \
-	ExynosVirtualDisplayModule.cpp
-
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE := libvirtualdisplaymodule
-include $(BUILD_SHARED_LIBRARY)
-
-endif
+LOCAL_SRC_FILES += \
+	./../../$(TARGET_SOC)/libvirtualdisplaymodule/ExynosVirtualDisplayModule.cpp
