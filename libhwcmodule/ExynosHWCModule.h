0bc7bd401f8dd35e93281aa1fa74453979217376
Salih Çağlar <salihcaglar4187@gmail.com>
2026-03-22 23:02:45 +0300

    libhwcmodule: Use stock AVAILABLE_GSC_UNITS value from decompiled MM BSP

--- libhwcmodule/ExynosHWCModule.h
+++ libhwcmodule/ExynosHWCModule.h
@@ -85,10 +85,6 @@ const size_t HDMI_GSC_IDX = 2;
 const size_t WFD_GSC_IDX = 3;
 #endif
 const int FIMD_GSC_USAGE_IDX[] = {FIMD_GSC_IDX, FIMD_GSC_SEC_IDX};
-#ifdef USES_VIRTUAL_DISPLAY
-const int AVAILABLE_GSC_UNITS[] = { 0, 1, 1, 1 };
-#else
-const int AVAILABLE_GSC_UNITS[] = { 0, 1, 1, 5 };
-#endif
+const int AVAILABLE_GSC_UNITS[] = { 0, 1, 1, 1, 5, 6 };
 
 #endif
